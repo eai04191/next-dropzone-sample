22ae7a1918ce7fc38c73c61e831e78efed56a33e
Yousei Takahashi <yousei.takahashi@ncdc.co.jp>
2023-04-17 16:58:53 +0900

    実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,137 +1,121 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import clsx from "clsx";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+function Row({ title }: { title: string }) {
+    const [isDragging, setIsDragging] = useState(false);
+    const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
+
+    function handleDragEnter() {
+        setIsDragging(true);
+    }
+    function handleDragLeave() {
+        setIsDragging(false);
+    }
+    function handleDragOver(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+    }
+    function handleDrop(e: React.DragEvent<HTMLDivElement>) {
+        // 新しいタブを開くのを防ぐ
+        e.preventDefault();
+
+        const files = Array.from(e.dataTransfer.files);
+        setUploadedFiles(files);
+
+        // debugger;
+        setIsDragging(false);
+    }
+
+    function handleClearItems() {
+        setUploadedFiles([]);
+    }
+
+    return (
+        <div
+            className={clsx(
+                "relative overflow-hidden rounded-lg border border-gray-300 bg-gray-100 p-4",
+                isDragging && "outline-dashed outline-4 outline-blue-400"
+            )}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+        >
+            <div
+                className={clsx(
+                    "flex flex-col gap-4",
+                    isDragging && "pointer-events-none"
+                )}
+            >
+                <div className="flex justify-between">
+                    <h2 className="text-2xl font-semibold">{title}</h2>
+                    <button
+                        className="rounded bg-gray-200 p-1 px-2"
+                        onClick={handleClearItems}
+                    >
+                        X
+                    </button>
+                </div>
+                <ul className="flex list-outside list-decimal flex-col gap-1 ps-7">
+                    {uploadedFiles.map((file) => (
+                        <li key={file.name}>{file.name}</li>
+                    ))}
+                </ul>
+            </div>
+
+            <div
+                className={clsx(
+                    "pointer-events-none",
+                    "absolute inset-0 flex items-center justify-center bg-white/75 p-4 text-center",
+                    !isDragging && "hidden"
+                )}
+            >
+                <span className="[&_span]:inline-block">
+                    <span>ここに</span>
+                    <span>ファイルを</span>
+                    <span>アップロード</span>
+                    <span>します</span>
+                </span>
+            </div>
+        </div>
+    );
+}
 
 export default function Home() {
+    const [rowCount, setRowCount] = useState(5);
+
+    function handleAddRow() {
+        setRowCount(rowCount + 1);
+    }
+    function handleRemoveRow() {
+        if (rowCount === 0) return;
+        setRowCount(rowCount - 1);
+    }
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    Get started by editing&nbsp;
-                    <code className="font-mono font-bold">
-                        src/pages/index.tsx
-                    </code>
-                </p>
-                <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-                    <a
-                        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
+        <main className="flex min-h-screen flex-col items-center justify-between gap-4 p-10 pt-6">
+            <div className="flex w-full items-center justify-between gap-3">
+                <h1 className="text-2xl font-bold">dropzone</h1>
+                <div className="flex items-center gap-3">
+                    <span>Row Count: </span>
+                    <button
+                        className="rounded bg-gray-100 p-2 px-6"
+                        onClick={handleAddRow}
                     >
-                        By{" "}
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            className="dark:invert"
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </a>
+                        +
+                    </button>
+                    <button
+                        className="rounded bg-gray-100 p-2 px-6"
+                        onClick={handleRemoveRow}
+                    >
+                        -
+                    </button>
                 </div>
             </div>
 
-            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-                <Image
-                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
-                    priority
-                />
-            </div>
-
-            <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-                <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2
-                        className={`${inter.className} mb-3 text-2xl font-semibold`}
-                    >
-                        Docs{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p
-                        className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-                    >
-                        Find in-depth information about Next.js features and
-                        API.
-                    </p>
-                </a>
-
-                <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2
-                        className={`${inter.className} mb-3 text-2xl font-semibold`}
-                    >
-                        Learn{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p
-                        className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-                    >
-                        Learn about Next.js in an interactive course
-                        with&nbsp;quizzes!
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2
-                        className={`${inter.className} mb-3 text-2xl font-semibold`}
-                    >
-                        Templates{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p
-                        className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-                    >
-                        Discover and deploy boilerplate example
-                        Next.js&nbsp;projects.
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2
-                        className={`${inter.className} mb-3 text-2xl font-semibold`}
-                    >
-                        Deploy{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p
-                        className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-                    >
-                        Instantly deploy your Next.js site to a shareable URL
-                        with Vercel.
-                    </p>
-                </a>
+            <div className="grid h-full w-full flex-1 auto-cols-fr grid-flow-col items-stretch gap-4">
+                {[...Array(rowCount)].map((_, i) => (
+                    <Row key={i} title={`Row ${i + 1}`} />
+                ))}
             </div>
         </main>
     );
